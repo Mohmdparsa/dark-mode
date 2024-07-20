d97566baf50d04fedf063239b3b3fe90d246fe60
mohammad parsapasand <mohmd.pars4@gmail.com>
2024-07-20 12:17:39 -0700

    style(App.js): change the color of text in dark mode and light mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+
 import DarkModeToggle from "./components/DarkModeToggle";
+
 const App = () => {
   return (
     <div className="bg-gray-100 dark:bg-gray-900 min-h-screen flex items-center justify-center">
-      <DarkModeToggle />
-      <p className="text-lg">Toggle Dark Mode</p>
+      <div className="text-center">
+        <DarkModeToggle />
+        <p className="text-lg text-gray-900 dark:text-white mt-4">Toggle Dark Mode</p>
+      </div>
     </div>
   );
 };
 
 export default App;
+
